6ffbab89725ab8e1f32a6967694a1f0483ce08d5
dxxbletriiiple <dxxbletriiiple@gmail.com>
2022-01-21 06:10:52 +0300

    fewer lines of code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 module.exports = function reverse(n) {
     if (n < 0) n = -n;
-    n += "";
-    n = n.split("").reverse().join("");
-    return +n;
+    return +(n + "").split("").reverse().join("");
 };
